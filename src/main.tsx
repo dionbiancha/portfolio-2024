5e9fa88da5212d4d5bf3997f4656b992cc06ad07
Dionei Bianchati <dion.biancha@gmail.com>
2024-06-10 10:24:03 -0300

    feat: analitc

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { DataProvider } from "./context/DataContext.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <DataProvider>
+      <Analytics />
       <App />
     </DataProvider>
   </React.StrictMode>
